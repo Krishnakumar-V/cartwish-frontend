f2a1f125b24bf5bdc02779ac2a4d452f8f803c53
Krishnakumar-V <19eucs069@skcet.ac.in>
2024-05-02 17:30:20 +0530

    Initial commit

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import "./LoginPage.css";
+import { getUser, login } from "../../services/userServices";
+import { Navigate, useLocation } from "react-router-dom";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email({ message: "Please enter valid email address" })
+    .min(3),
+  password: z
+    .string()
+    .min(8, { message: "Password should be at least 8 characters" }),
+});
+
+const LoginPage = () => {
+  const [formError, setformError] = useState("");
+  const location = useLocation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onSubmit = async (formData) => {
+    try {
+      await login(formData);
+      const { state } = location;
+      window.location = state ? state.form : "/";
+    } catch (err) {
+      if (err.response && err.response.status === 400) {
+        setformError(err.response.data.message);
+      }
+    }
+  };
+
+  if (getUser()) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <section className="align_center form_page">
+      <form className="authentication_form" onSubmit={handleSubmit(onSubmit)}>
+        <h2>Login Form</h2>
+        <div className="form_inputs">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form_text_input"
+              placeholder="Enter your email"
+              {...register("email")}
+            />
+            {errors.email === "required" && (
+              <em className="form_error">{errors.email.message}</em>
+            )}
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              className="form_text_input"
+              placeholder="Enter your password"
+              {...register("password")}
+            />
+            {errors.password === "required" && (
+              <em className="form_error">{errors.password.message}</em>
+            )}
+          </div>
+          {formError && <em className="form_error">{formError}</em>}
+          <button type="submit" className="form_submit search_button">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/Home/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import iphone from "../../assets/iphone-14-pro.webp";
+import mac from "../../assets/mac-system-cut.jfif";
+import HeroSection from "./HeroSection";
+import FeaturedProducts from "./FeaturedProducts";
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroSection
+        title="Buy iPhone 14 pro"
+        subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever"
+        link="/product/66292ff080f0f3c80ac1731a"
+        image={iphone}
+      />
+      <FeaturedProducts />
+      <HeroSection
+        title="Build the ultimate setup"
+        subtitle="You can add Studio Display and colour matched Magic accessories to your bag after configure you Mac mini"
+        link="/product/66292ff080f0f3c80ac17322"
+        image={mac}
+      />
+    </div>
+  );
+};
+
+export default HomePage;
